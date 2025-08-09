e75d3939789bc42b10415db0e6b7c34bb0e8d973
cometre <223025589+cometre@users.noreply.github.com>
2025-08-09 11:20:52 +0300

    maintenance [202508091119]

--- release.sh
+++ release.sh
@@ -10,22 +10,43 @@ fi
 
 now=$(date +%Y%m%d%H%M)
 
+delete_first_line() {
+    local file="$1"
+
+    if sed --version >/dev/null 2>&1; then
+        # GNU sed (Linux)
+        sed -i "1d" "$file"
+    else
+        # BSD sed (macOS)
+        sed -i '' "1d" "$file"
+    fi
+}
+
+update_timestamp() {
+    local file="$1"
+    if sed --version >/dev/null 2>&1; then
+        # GNU sed (Linux)
+        sed -i "1i# UPDATED:$now" "$file"
+    else
+        # BSD sed (macOS)
+        sed -i '' "1i\\
+# UPDATED:$now
+" "$file"
+    fi
+}
+
+delete_first_line sr_direct_ru_geo_and_asn.conf
+update_timestamp sr_direct_ru_geo_and_asn.conf
+delete_first_line sr_reject_ru_geo_and_asn.conf
+update_timestamp sr_reject_ru_geo_and_asn.conf
+
 git add *.list
+git add *.conf
 git commit -m "update lists [$now]"
 git push origin master
 if [ $? -ne 0 ]; then
   echo "Failed to push changes to remote repository."
   exit 1
 fi
-
-sed -i '' "1d" sr_direct_ru_geo_and_asn.conf
-sed -i '' "1i\\
-# UPDATED:$now
-" sr_direct_ru_geo_and_asn.conf
-
-sed -i '' "1d" sr_reject_ru_geo_and_asn.conf
-sed -i '' "1i\\
-# UPDATED:$now
-" sr_reject_ru_geo_and_asn.conf
 
 echo "Changes pushed successfully."
